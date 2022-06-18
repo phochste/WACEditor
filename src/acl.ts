d165684c7ab8e46286bc6dc5877ed6045fcb5eaf
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-06-18 19:19:52 +0200

    Adding WAC access

--- src/acl.ts
+++ src/acl.ts
@@ -1,4 +1,8 @@
-import { universalAccess } from "@inrupt/solid-client";
+import { 
+   universalAccess ,  
+   getSolidDatasetWithAcl,
+   getPublicAccess,
+   getAgentAccessAll} from "@inrupt/solid-client";
 import { fetch } from '@inrupt/solid-client-authn-browser';
 
 export type ACLType = {
@@ -10,15 +14,52 @@ export type ACLType = {
 };
 
 export async function getAcl(resource: string) : Promise<ACLType[] | null> {
-   return getUniversalACL(resource);
+   //return getUniversalACL(resource);
+   return getWACACL(resource);
 }
 
 async function getWACACL(resource: string) : Promise<ACLType[] | null> {
-    return null;
+   console.log(`getWACACL(${resource})`);
+
+   const acls : ACLType[] = [];
+
+   try {
+      const myDatasetWithAcl = await getSolidDatasetWithAcl(resource, {
+         fetch: fetch
+      });
+      const publicAccess = getPublicAccess(myDatasetWithAcl);
+
+      acls.push( {
+         agent: '#public' ,
+         read: publicAccess['read'] ,
+         write: publicAccess['write'] ,
+         append: publicAccess['append'] ,
+         control: publicAccess['control']
+      } as ACLType);
+
+      const accessByAgent = getAgentAccessAll(myDatasetWithAcl);
+
+      for (const agent in accessByAgent) {
+         acls.push( {
+            agent: agent ,
+            read: accessByAgent[agent]['read'] ,
+            write: accessByAgent[agent]['write'] ,
+            append: accessByAgent[agent]['append'] ,
+            control: accessByAgent[agent]['control']
+         } as ACLType);
+      }
+   }
+   catch (e) {
+      console.log(`WAC Error ${e}`);
+      return null;
+   }
+
+   return acls;
 }
 
 async function getUniversalACL(resource:string) : Promise<ACLType[] | null> {
    console.log(`getUniversalACL(${resource})`);
+
    const acls : ACLType[] = [];
 
    try {
@@ -48,8 +89,8 @@ async function getUniversalACL(resource:string) : Promise<ACLType[] | null> {
             } as ACLType);
         }
    }
-   catch(ex) {
-      console.log(ex);
+   catch(e) {
+      console.log(`Universal error ${e}`);
       return null;
    }
 
@@ -57,7 +98,7 @@ async function getUniversalACL(resource:string) : Promise<ACLType[] | null> {
 }
 
 export async function setAcl(resource: string, agent: string, field : string, value: boolean) : Promise<boolean | null> {
-   console.log(`set(${agent},${field},${value})`);
+   console.log(`set(${resource},${agent},${field},${value})`);
 
    let result : any;
    try {
